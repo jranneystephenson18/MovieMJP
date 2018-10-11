2bb703fcd5d785310b2d64ddd77b21db39d9c3fd
Praveen Tetali <praveentetali35@Gmail.com>
2018-10-11 18:36:01 -0400

    reverting commit

---
+++ MidtermMovie/Movies.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermMovie
+{
+    class Movies
+    {
+        public List<Movie> MoviesList = new List<Movie>();
+
+       public void AddMovie()
+        {
+            Movie movie = new Movie();
+            Console.WriteLine("Enter movie name: ");
+            movie.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter actor name: ");
+            movie.Actor = Console.ReadLine();
+
+            Console.WriteLine("Enter director name: ");
+            movie.Director = Console.ReadLine();
+
+            Console.WriteLine("Enter genre: ");
+            movie.Genre = Console.ReadLine();
+            Console.WriteLine();
+            this.MoviesList.Add(movie);
+        }
+
+        public Movie FindMovie()
+        {
+            Console.WriteLine("Enter movie name: ");
+            string moviename = Console.ReadLine();
+            return this.MoviesList.FirstOrDefault(m => m.Name.ToLower() == moviename.ToLower());
+        }
+
+        public Movie FindMovieByActor()
+        {
+            Console.WriteLine("Enter actor name: ");
+            string actorname = Console.ReadLine();
+            return this.MoviesList.FirstOrDefault(m => m.Actor.ToLower() == actorname.ToLower());
+        }
+
+        public Movie FindMovieByDirector()
+        {
+            Console.WriteLine("Enter director name: ");
+            string directorname = Console.ReadLine();
+            return this.MoviesList.FirstOrDefault(m => m.Director.ToLower() == directorname.ToLower());
+        }
+
+        public void DisplayMovie(Movie movie)
+        {
+            if(movie==null)
+            {
+                Console.WriteLine("No movie found.");
+            }
+            else
+            {
+                Console.WriteLine("The movie's details are:  ");
+                Console.WriteLine("Name :  " + movie.Name);
+                Console.WriteLine("Actor :  " + movie.Actor);
+                Console.WriteLine("Director :  " + movie.Director);
+                Console.WriteLine("Genre :  " + movie.Genre);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- MidtermMovie/Program.cs
+++ MidtermMovie/Program.cs
@@ -10,7 +10,59 @@ namespace MidtermMovie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Movies mvoies = new Movies();
+            int input;
+            do
+            {
+                input = 0;
+                Console.WriteLine("Choose any one option.....");
+                Console.WriteLine("1. Find a movie");
+                Console.WriteLine("2. Find movie by actor");
+                Console.WriteLine("3. Find a movie by director");
+                Console.WriteLine("4. Add a new movie");
+                Console.WriteLine("5. Quit Program");
+
+                string inputstring = Console.ReadLine();
+                bool iscorrectiput = int.TryParse(inputstring, out input);
+
+                while (!iscorrectiput && input < 6 && input > 0)
+                {
+                    Console.WriteLine("Invalid option, Please try again");
+                    iscorrectiput = int.TryParse(inputstring, out input);
+                }
+
+                Movie searchedmovie;
+                switch (input)
+                {
+                    case 1:
+                        searchedmovie = mvoies.FindMovie();
+                        mvoies.DisplayMovie(searchedmovie);
+                        break;
+
+                    case 2:
+                        searchedmovie = mvoies.FindMovieByActor();
+                        mvoies.DisplayMovie(searchedmovie);
+                        break;
+
+                    case 3:
+                        searchedmovie = mvoies.FindMovieByDirector();
+                        mvoies.DisplayMovie(searchedmovie);
+                        break;
+
+                    case 4:
+                        mvoies.AddMovie();
+                        
+                        break;
+
+                    case 5:
+                        Console.WriteLine("The program is ended..");
+                        break;
+                }
+
+                Console.WriteLine("=============================================================");
+            } while (input!=5);
+            
+           
         }
     }
 }
